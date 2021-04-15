0c9a9f03f182f63393bee677f5ed53f5044e8439
Vasil Paligorov <36786687+VasilPaligorov@users.noreply.github.com>
2021-04-15 21:34:38 +0300

    Added React to our project (#4)
    Co-authored-by: Vasil Paligorov <vpaligorov@pop-os.localdomain>

---
+++ flaskProject1/static/js/Register/Register1.js
@@ -0,0 +1,16 @@
+class Register1 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <>
+
+            </>
+        )
+    }
+}
