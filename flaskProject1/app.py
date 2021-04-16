c56b16bafa087687d4b1b3ca5e7f03c7e1dd95c8
kris <krisvas8aelsys@gmail.com>
2021-04-16 14:52:42 +0300

    Replacement of MongoDB Login/Registration

--- flaskProject1/app.py
+++ flaskProject1/app.py
@@ -1,59 +1,107 @@
-from flask import Flask, render_template, url_for, request, session, redirect
-from flask_pymongo import PyMongo
+from flask import Flask, render_template, request, url_for, redirect, session
+import pymongo
 import bcrypt
 
 app = Flask(__name__)
-app.secret_key = 'mysecretkey'
-
-# app.config['SESSION_TYPE'] = 'filesystem'
-app.config['MONGO_DBNAME'] = 'mongologinexample'
-app.config['MONGO_URI'] = 'mongodb+srv://kris:1234567890@cluster0.vjdqy.mongodb.net/mongologinexample'
-
-mongo = PyMongo(app)
+app.secret_key = "testing"
+client = pymongo.MongoClient("mongodb+srv://kris:1234567890@cluster0.vjdqy.mongodb.net/mongotest?retryWrites=true&w=majority")
+db = client.get_database('total_records')
+records = db.register
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
+  
+@app.route("/register", methods=['post', 'get'])
+def index():
 
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    response = None
-    if request.method == 'GET':
-        return render_template('login1.html')
+    message = ''
+    if "email" in session:
+        return redirect(url_for("logged_in"))
 
-    else:
-        users = mongo.db.users
-        login_user = users.find_one({'name': request.form['username']})
+    if request.method == "POST":
+        user = request.form.get("fullname")
+        email = request.form.get("email")
+        
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+        
+        user_found = records.find_one({"name": user})
+        email_found = records.find_one({"email": email})
+        if user_found:
+            message = 'There already is a user by that name'
+            return render_template('register1.html', message=message)
+        if email_found:
+            message = 'This email already exists in database'
+            return render_template('register1.html', message=message)
+        if password1 != password2:
+            message = 'Passwords should match!'
+            return render_template('register1.html', message=message)
 
-        if login_user:
-            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
-            if bcrypt.hashpw(bytes(request.form['pass'], 'utf-8'), hashpass) == hashpass:
-            # if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
-                session['username'] = request.form['username']
-                return redirect(url_for('register'))
-
-        return 'Invalid username/password combination'
-
-
-@app.route('/register', methods=['POST', 'GET'])
-def register():
-    if request.method == 'POST':
-        users = mongo.db.users
-        existing_user = users.find_one({'name': request.form['username']})
-
-        if existing_user is None:
-            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
-            users.insert({'name': request.form['username'], 'password': hashpass})
-            session['username'] = request.form['username']
-            return redirect(url_for('login'))
-
-        return 'That username already exists!'
-
+        else:
+            hashed = bcrypt.hashpw(password2.encode('utf-8'), bcrypt.gensalt())
+            user_input = {'name': user, 'email': email, 'password': hashed}
+            records.insert_one(user_input)
+            
+            user_data = records.find_one({"email": email})
+            new_email = user_data['email']
+   
+            return render_template('logged_in.html', email=new_email)
     return render_template('register1.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    message = 'Please login to your account'
+    if "email" in session:
+        return redirect(url_for("logged_in"))
+        
 
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+       
+        email_found = records.find_one({"email": email})
+        if email_found:
+            email_val = email_found['email']
+            passwordcheck = email_found['password']
+            
+            if bcrypt.checkpw(password.encode('utf-8'), passwordcheck):
+                session["email"] = email_val
+                return redirect(url_for('logged_in'))
+            else:
+                if "email" in session:
+                    return redirect(url_for("logged_in"))
+                message = 'Wrong password'
+                return render_template('login1.html', message=message)
+        else:
+            message = 'Email not found'
+            return render_template('login1.html', message=message)
+    return render_template('login1.html', message=message)
+
+
+@app.route('/logged_in')
+def logged_in():
+    if "email" in session:
+        email = session["email"]
+        return render_template('logged_in.html', email=email)
+    else:
+        return redirect(url_for("login"))
+
+
+
+@app.route("/logout", methods=["POST", "GET"])
+def logout():
+    if "email" in session:
+        session.pop("email", None)
+        return render_template("signout.html")
+    else:
+        return render_template('index.html')
+        
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
--- flaskProject1/static/js/Login/Login1.js
+++ flaskProject1/static/js/Login/Login1.js
@@ -9,15 +9,19 @@ class Login1 extends React.Component {
     render() {
         return (
             <>
-                <div className="form">
-                    <h2>Log In</h2>
-                    <form className="login-form">
-                        <input type="text" placeholder="username"/>
-                        <input type="password" placeholder="password"/>
-                        <button>login</button>
-                        <p className="message">Not registered? <a href="/register">Create an account</a></p>
-                    </form>
+                <form action="" method="post">
+                    <div class="form-group">
+                        <label for="InputEmail">Email address</label>
+                        <input name="email" class="form-control" id="InputEmail"  placeholder="Enter email"/>
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div class="form-group">
+                    <label for="InputPassword">Password</label>
+                    <input type="password" name="password" class="form-control" id="InputPassword" placeholder="Password"/>
                 </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+            
             </>
         )
     }
