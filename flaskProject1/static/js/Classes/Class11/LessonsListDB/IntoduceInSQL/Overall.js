e8d36a330f8a9fb2d5f43ad5520942a5bf4ac93d
Vasil Paligorov <36786687+VasilPaligorov@users.noreply.github.com>
2021-04-28 13:36:58 +0300

    css in test form improved but not finished yet (#16)

--- flaskProject1/static/js/Classes/Class11/LessonsListDB/IntoduceInSQL/Overall.js
+++ flaskProject1/static/js/Classes/Class11/LessonsListDB/IntoduceInSQL/Overall.js
@@ -12,13 +12,15 @@ class DBList extends React.Component {
         return (
             <>
             <h1>Въведение в SQL</h1>
+
+            <div id = "video_exercise">
                 <h2>Видео  Урок:</h2>
-                <br/>
-                <br/>
-            <div id = "video_exercise">                
-            <iframe width="960" height="540" src="https://www.youtube.com/embed/2bW3HuaAUcY" title="YouTube video player" 
-            frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-             allowfullscreen></iframe>
+
+                <a href="#Test" id="test_button">Тест върху материала</a>
+
+                <iframe width="960" height="540" src="https://www.youtube.com/embed/2bW3HuaAUcY" title="YouTube video player"
+                frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                 allowfullscreen></iframe>
                  
                 <h2> Упражнение:</h2>
 
