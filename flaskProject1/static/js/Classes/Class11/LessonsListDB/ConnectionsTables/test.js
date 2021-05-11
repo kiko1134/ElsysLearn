5dd93fdb76b024fdb985e84a0a66bed7b1f53dfd
kris <krisvas8aelsys@gmail.com>
2021-05-12 00:43:55 +0300

    added test for connections tables

--- flaskProject1/static/js/Classes/Class11/LessonsListDB/ConnectionsTables/test.js
+++ flaskProject1/static/js/Classes/Class11/LessonsListDB/ConnectionsTables/test.js
@@ -15,11 +15,11 @@ class Test extends React.Component {
         var q6 = document.quiz.question6.value;
         var grade = 0;
 
-        if (q1 === "Option3"){grade++}
-        if (q2 === "Option2"){grade++}
-        if (q3 === "Option1"){grade++}
-        if (q4 === "Option2"){grade++}
-        if (q5 === "Option3"){grade++}
+        if (q1 === "Option4"){grade++}
+        if (q2 === "Option1"){grade++}
+        if (q3 === "Option2"){grade++}
+        if (q4 === "Option3"){grade++}
+        if (q5 === "Option1"){grade++}
         if (q6 === "Option2"){grade++}
 
         if (grade>4){
@@ -38,51 +38,51 @@ class Test extends React.Component {
         return (
             <form name="quiz" id="quiz">
                 <div id="question">
-                    <p>1. Колко на брой са агрегатните функции, които разглеждаме в този видео урок?</p>
-                    <p><input type="radio" name="question1" value="Option1"/>a) 2</p>
-                    <p><input type="radio" name="question1" value="Option2"/>b) 3</p>
-                    <p><input type="radio" name="question1" value="Option3"/>c) 5</p>
-                    <p><input type="radio" name="question1" value="Option4"/>d) Нито един от посочените</p>
+                    <p>1. Коя от долупосочените е връзка между таблици?</p>
+                    <p><input type="radio" name="question1" value="Option1"/>a) One to many</p>
+                    <p><input type="radio" name="question1" value="Option2"/>b) One to one</p>
+                    <p><input type="radio" name="question1" value="Option3"/>c) Many to many</p>
+                    <p><input type="radio" name="question1" value="Option4"/>d) И трите отговора са верни</p>
                 </div>
 
                 <div id="question">
-                    <p>2. Кои от изброените по долу са агрегатни функции?</p>
-                    <p><input type="radio" name="question2" value="Option1"/>a) SUM</p>
-                    <p><input type="radio" name="question2" value="Option2"/>b) MULT</p>
-                    <p><input type="radio" name="question2" value="Option3"/>c) MIN</p>
-                    <p><input type="radio" name="question2" value="Option4"/>d) MAX</p>
+                    <p>2. Как имплементираме връзката One to one?</p>
+                    <p><input type="radio" name="question2" value="Option1"/>a) Като направим unique foreign key във втората таблица</p>
+                    <p><input type="radio" name="question2" value="Option2"/>b) Като направим foreign key във втората таблица</p>
+                    <p><input type="radio" name="question2" value="Option3"/>c) Като си направим трета таблица, чрез която се осъществява връзката ,между главните две</p>
+                    <p><input type="radio" name="question2" value="Option4"/>d) Нито един от посочените</p>
                 </div>
 
                 <div id="question">
-                    <p>3. Коя заявка е написана правилно?</p>
-                    <p><input type="radio" name="question3" value="Option1"/>a) select COUNT() from table;</p>
-                    <p><input type="radio" name="question3" value="Option2"/>b) get count() from table;</p>
-                    <p><input type="radio" name="question3" value="Option3"/>c) select *COUNT() from table;</p>
-                    <p><input type="radio" name="question3" value="Option4"/>d) select COUNT() where condition;</p>
+                    <p>3. Как имплементираме връзката Many to many?</p>
+                    <p><input type="radio" name="question2" value="Option1"/>a) Като направим unique foreign key във втората таблица</p>
+                    <p><input type="radio" name="question2" value="Option2"/>b) Като направим foreign key във втората таблица</p>
+                    <p><input type="radio" name="question2" value="Option3"/>c) Като си направим трета таблица, чрез която се осъществява връзката ,между главните две</p>
+                    <p><input type="radio" name="question2" value="Option4"/>d) Нито един от посочените</p>
                 </div>
 
                 <div id="question">
-                    <p>4.Какво прави агрегатната функция AVG?</p>
-                    <p><input type="radio" name="question4" value="Option1"/>a) Взима средно аритметично от целия ред</p>
-                    <p><input type="radio" name="question4" value="Option2"/>b) Взима средно аритметично от цялата колона</p>
-                    <p><input type="radio" name="question4" value="Option3"/>c) Не прави нищо</p>
-                    <p><input type="radio" name="question4" value="Option4"/>d) Нито едно от посочените</p>
+                    <p>4. Как имплементираме връзката One to many?</p>
+                    <p><input type="radio" name="question2" value="Option1"/>a) Като направим unique foreign key във втората таблица</p>
+                    <p><input type="radio" name="question2" value="Option2"/>b) Като направим foreign key във втората таблица</p>
+                    <p><input type="radio" name="question2" value="Option3"/>c) Като си направим трета таблица, чрез която се осъществява връзката ,между главните две</p>
+                    <p><input type="radio" name="question2" value="Option4"/>d) Нито един от посочените</p>
                 </div>
 
                 <div id="question">
-                    <p>5.Какво пишем когато искаме да зададем условност на агрегатна функция?</p>
-                    <p><input type="radio" name="question5" value="Option1"/>a) Не пишем нищо</p>
-                    <p><input type="radio" name="question5" value="Option2"/>b) when condition</p>
-                    <p><input type="radio" name="question5" value="Option3"/>c) where condition</p>
-                    <p><input type="radio" name="question5" value="Option4"/>d) нито едно от изброените</p>
+                    <p>5. Ако имаме задача, в която се казва, че един човек може да има много оценки, коя връзка трябва да изпозлваме?</p>
+                    <p><input type="radio" name="question2" value="Option1"/>a) One to many</p>
+                    <p><input type="radio" name="question2" value="Option2"/>b) One to one</p>
+                    <p><input type="radio" name="question2" value="Option3"/>c) Нито едно от посочените</p>
+                    <p><input type="radio" name="question2" value="Option4"/>d) Many to many</p>
                 </div>
 
                 <div id="question">
-                    <p>6.Какъв тип база е MySQL?</p>
-                    <p><input type="radio" name="question6" value="Option1"/>a) Hierarchical database</p>
-                    <p><input type="radio" name="question6" value="Option2"/>b) Relational database</p>
-                    <p><input type="radio" name="question6" value="Option3"/>c) Document database</p>
-                    <p><input type="radio" name="question6" value="Option4"/>d) NoSQL database</p>
+                    <p>5. Ако имаме задача, в която се казва, че един човек може да има само една оценка, коя връзка трябва да изпозлваме?</p>
+                    <p><input type="radio" name="question2" value="Option1"/>a) One to many</p>
+                    <p><input type="radio" name="question2" value="Option2"/>b) One to one</p>
+                    <p><input type="radio" name="question2" value="Option3"/>c) Нито едно от посочените</p>
+                    <p><input type="radio" name="question2" value="Option4"/>d) Many to many</p>
                 </div>
 
                 <input type="button" name="" value="I'm done!" id="button" onClick={this.Check}/>
