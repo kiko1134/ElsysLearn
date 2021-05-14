e00324edb8a15c6b690d7dd8a0f8abe3a7621cdf
VasilPaligorov <vpaligorov@gmail.com>
2021-05-14 16:45:47 +0300

    Like Button added

--- flaskProject1/static/js/Classes/Class11/LessonsListDB/JoinQueries/Overall.js
+++ flaskProject1/static/js/Classes/Class11/LessonsListDB/JoinQueries/Overall.js
@@ -5,7 +5,28 @@ class DBList extends React.Component {
 
         }
     }
-    
+
+    Liked(){
+        const likeBtn = document.querySelector(".like__btn");
+        let likeIcon = document.querySelector("#icon"),
+          count = document.querySelector("#count");
+
+        let clicked = false;
+
+
+        likeBtn.addEventListener("click", () => {
+          if (!clicked) {
+            clicked = true;
+            likeIcon.innerHTML = `<i class="fas fa-thumbs-up"></i>`;
+            count.textContent++;
+          } else {
+            clicked = false;
+            likeIcon.innerHTML = `<i class="far fa-thumbs-up"></i>`;
+            count.textContent--;
+          }
+
+        });
+    }
 
    
     render() {
@@ -21,17 +42,19 @@ class DBList extends React.Component {
                 <iframe width="960" height="540" src="https://www.youtube.com/embed/2bW3HuaAUcY" title="YouTube video player"
                 frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                  allowfullscreen></iframe>
-                 
-                {/* <button class="like__btn">
+
+                <button class="like__btn" onClick={this.Liked}>
                     <span id="icon"><i class="far fa-thumbs-up"></i></span>
                     <span id="count">0</span> Like
                 </button>
 
-                <script src="/home/kris/Desktop/ElsysLearn/flaskProject1/static/js/button.js"> </script> */}
+                {/*<script src="/home/kris/Desktop/ElsysLearn/flaskProject1/static/js/button.js"></script>*/}
+                <div id="upr">
                 <h2> Упражнение:</h2>
 
                 <div id="Test">
 
+                </div>
                 </div>
             </div>
             </>
